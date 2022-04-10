a1c61caf33f45963682713d784c7c064d9253705
Andrew Hunt <ahunt.tesselation@gmail.com>
2022-04-09 18:49:07 -0700

    Updating

--- main.go
+++ main.go
@@ -9,45 +9,63 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
-	// "bufio"
 	"text/template"
 	"bytes"
+
+	"github.com/fatih/color"
 )
 
 var installPath string = "/opt/AppImage"
+var iconPath string = "/usr/share/icons/AppImage"
 
 func main() {
-	// reader := bufio.NewReader(os.Stdin)
+	// Show help if program run without paramters
+	if len(os.Args) == 1 {
+		ShowHelp()
+		os.Exit(0)
+	}
 
 	switch args := os.Args[1]; args {
 	case "install":
+		if len(os.Args) == 2 {
+			fmt.Println("Please specify an AppImage to install")
+			os.Exit(1)
+		}
+
 		if CheckForRoot() == false {
 			fmt.Println("Need root for install operation. Please rerun desktopify with sudo")
+			os.Exit(1)
 		}
+		
 		CheckInstallDirectory()
 		appImageSrc := TransformFile(os.Args[2])
 		appImageDest := GetDestinationPath(appImageSrc)
 		CopyFile(appImageSrc, appImageDest)
+		iconLocation := ExtractIcon(appImageSrc)
+		GenerateShortcut(os.Args[2], iconLocation)
 		fmt.Println("Installed AppImage to", appImageDest)
 	case "list":
 		ListAppImages()
 	case "remove":
+		if CheckForRoot() == false {
+			fmt.Println("Need root for removal operation. Please rerun desktopify with sudo")
+			os.Exit(1)
+		}
 		fmt.Printf("This will remove %s from the install directory. Confirm y/n \n", os.Args[2])
-
 		var s string
 		_, err := fmt.Scan(&s)
 		CheckError(err)
-		// response, _ := reader.ReadString('\n')
-		// RemoveAppImage(os.Args[2])
-		if s == "y" {
+		if s == "y" || s == "yes" {
 			RemoveAppImage(os.Args[2])
+			fmt.Printf("%s removed from system\n", os.Args[2])
 		} else {
+			fmt.Println("Aborted")
 			os.Exit(1)
 		}
 	case "debug":
-		GenerateShortcut("Program")
+		ExtractIcon(os.Args[2])
 	default:
-		fmt.Println("Unrecognized input")
+		ShowHelp()
 	}
 }
 
@@ -55,7 +73,12 @@ func CheckInstallDirectory() {
 	_, err := os.Stat(installPath)
 	if os.IsNotExist(err) {
 		fmt.Println("Installation directory /opt/AppImage created")
-		_ = os.Mkdir(installPath, 0665)
+		_ = os.Mkdir(installPath, 0775)
+	}
+	_, err = os.Stat(iconPath)
+	if os.IsNotExist(err) {
+		fmt.Println("Icon directory /usr/share/icons created")
+		_ = os.Mkdir(iconPath, 0775)
 	}
 }
 
@@ -98,25 +121,49 @@ func GetDestinationPath(file string) string {
 
 func CopyFile(src string, dst string) {
 	source, err := os.Open(src)
-	defer source.Close()
 	destination, err := os.Create(dst)
-	defer destination.Close()
 	_, err = io.Copy(destination, source)
 	CheckError(err)
+	source.Close()
+	destination.Close()
+	os.Chmod(dst, 0775)
 }
 
 func ListAppImages() {
 	files, err := ioutil.ReadDir(installPath)
 	CheckError(err)
 	for _, file := range files {
-		fmt.Println(file.Name())
+		if strings.Contains(file.Name(), "AppImage"){
+				fmt.Println(file.Name())
+		}
 	}
 }
 
 func RemoveAppImage(name string) {
+	// Remove AppImage
 	file := installPath + "/" + name
-	fmt.Println(file)
 	err := os.Remove(file)
+	CheckError(err)
+	// Remove desktop shortcut
+	s := strings.Split(name, "/")
+	AppImageName := s[len(s)-1]
+	programName := strings.Split(AppImageName, "-")
+	desktopFile := "/home/" + os.Getenv("SUDO_USER") + "/.local/share/applications/" + programName[0] + ".desktop"
+	err = os.Remove(desktopFile)
+	CheckError(err)
+
+	files, err := ioutil.ReadDir(iconPath)
+	CheckError(err)
+
+	var iconFile string
+
+	for _, file := range files {
+		if strings.Contains(file.Name(), programName[0]) {
+			iconFile = file.Name()
+		}
+	}
+
+	err = os.Remove(iconPath + "/" + iconFile)
 	CheckError(err)
 }
 
@@ -127,10 +174,10 @@ type Shortcut struct {
 	DesktopFile string
 }
 
-func GenerateShortcut(file string) {
+func GenerateShortcut(file string, iconLocation string) {
+	fileName := TransformFile(file)
 	st := Shortcut{}
-	st.DesktopFile =`
-[Desktop Entry]
+	st.DesktopFile =`[Desktop Entry]
 Name={{.Name}}
 Comment={{.Name}}
 Icon={{.Icon}}
@@ -138,14 +185,70 @@ Exec={{.ExecFile}}
 Terminal=false
 Type=Application
 `
-	st.Name = "Demo"
-	st.Icon = "Demo"
-	st.ExecFile = "Demo"
+	s := strings.Split(fileName, "/")
+	name := s[len(s)-1]
+	programName := strings.Split(name, "-")
+	st.Name = programName[0]
+	st.Icon = iconLocation
+	st.ExecFile = GetDestinationPath(fileName)
 
-	fmt.Println(st.DesktopFile)
 	var b bytes.Buffer
 	t, err := template.New("").Parse(st.DesktopFile)
 	CheckError(err)
 	err = t.Execute(&b, st)
-	fmt.Println(b.String())
+	desktopFile := "/home/" + os.Getenv("SUDO_USER") + "/.local/share/applications/" + programName[0] + ".desktop"
+	by := []byte(b.String())
+	f, err := os.Create(desktopFile)
+	f.Write(by)
+	f.Close()
+	os.Chmod(desktopFile, 0775)
+	CheckError(err)
+}
+
+func ExtractIcon(appImageFile string) string {
+	var srcIconFile string
+	var destIconFile string
+
+	os.Chmod(appImageFile, 0775)
+	cmd := exec.Command(appImageFile, "--appimage-extract")
+	cmd.Dir = "/tmp"
+	cmd.Run()
+	files, err := ioutil.ReadDir("/tmp/squashfs-root")
+	CheckError(err)
+
+	var iconFile string
+	for _, file := range files {
+		if strings.Contains(file.Name(), ".png") {
+			iconFile = file.Name()
+		}
+		if strings.Contains(file.Name(), ".svg") {
+			iconFile = file.Name()
+		}
+	}
+
+	iconFileSplit := strings.Split(iconFile, ".")
+
+	s := strings.Split(appImageFile, "/")
+	name := s[len(s)-1]
+	programName := strings.Split(name, "-")
+
+	srcIconFile = "/tmp/squashfs-root/" + iconFile
+	destIconFile = iconPath + "/" + programName[0] + "." + iconFileSplit[1]
+
+	err = os.Rename(srcIconFile, destIconFile)
+	CheckError(err)
+	return destIconFile
+}
+
+func ShowHelp() {
+	color.Cyan("Desktopify - Your AppImage Installer")
+	fmt.Printf("\n")
+	fmt.Println("List images")
+	color.Green("desktopify list")
+	fmt.Printf("\n")
+	fmt.Println("Install image")
+	color.Green("desktopify install Software.AppImage")
+	fmt.Printf("\n")
+	fmt.Println("Remove image")
+	color.Green("desktopify remove Software.AppImage")
 }
